4be794225193007e2d39215a300ab70939ca53fd
DeweyBanks <DeweyBanks@gmail.com>
2014-10-29 20:53:47 -0400

    added logout and footer to every page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,8 +46,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     @user.save
-    session[:user_id] = @user.ids
-    redirect_to(@user)
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
   end
 
   # PATCH/PUT /users/1
