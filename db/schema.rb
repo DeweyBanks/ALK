9f4e0d71e6e79be782d6d07cf78592a493b9d6b2
DeweyBanks <DeweyBanks@gmail.com>
2014-11-18 19:31:44 -0500

    creates courses table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141029184712) do
+ActiveRecord::Schema.define(version: 20141118235044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: true do |t|
+    t.string   "title"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.integer  "kid_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "kids", force: true do |t|
     t.string   "name",                                     null: false
